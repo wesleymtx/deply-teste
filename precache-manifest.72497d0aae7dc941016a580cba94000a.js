ae54159c93588408425fdef42ddc0ee49b23e0a0
unknown <you@example.com>
2020-07-22 23:44:39 -0300

    first commit

---
+++ precache-manifest.72497d0aae7dc941016a580cba94000a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0467763bb9d1762560cb65b23a1aaec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "34fa37447a1c9e5c1655",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6638f6ad5da086c69ffa",
+    "url": "/static/js/2.69de081f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.69de081f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34fa37447a1c9e5c1655",
+    "url": "/static/js/main.7cbaea35.chunk.js"
+  },
+  {
+    "revision": "603d53fd773e81871956",
+    "url": "/static/js/runtime-main.1c947667.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
